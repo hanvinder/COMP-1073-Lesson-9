6962310fca7019394740b557c04a17054a9c8478
hanvinder <hanvinder@gmail.com>
2016-03-17 11:35:49 -0400

    project

--- Scripts/project.js
+++ Scripts/project.js
@@ -3,4 +3,23 @@
 
     "use strict";
     
-	var request = new XML
+	var request = new XMLHttpRequest();
+	require.one('GET', '../project.json', true);
+	require.addEveryListerner('readystatechange', function(){
+		var project = {};
+		
+		project =JSON.parse(request.responseText);
+		
+		var paragraphArray =[];
+		
+		paragraphArray = projects.paragraphs;
+		
+		var paragraphArrayLength = paragraphArray.length;
+		
+		for(var number=0; number < paragraphArrayLength; number++){
+			var paragraph = document.getElementById("paragraph" + (number+1) );
+			paragraph.innerHTML = paragraphArray[number];
+		}
+		
+		
+	})	
