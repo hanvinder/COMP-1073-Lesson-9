1f8df935006e51cfce322cb27b82849f83e325d6
hanvinder <hanvinder@gmail.com>
2016-03-17 09:17:56 -0400

    update js file

--- Scripts/app.js
+++ Scripts/app.js
@@ -43,54 +43,52 @@ var person ={}; /*var Person = new Objedct();
 person.age = 40;
 
 console.log(person.age);*/
-var output = "";
+ var output = "";
     var firstParagraph = document.getElementById("firstParagraph");
 
-    var Person = {
-        "age": 20,
-        "name": "Hanvinder",
-        "address": "360 Duckworth",
-        "city": "Barrie",
-        "province": "Ontario",
-        "familyNames": [
-            "Mehtab",
-            "Satwinder",
-            "Jaswinder",
-            "Gurneet"
-        ],
-        "sayHello":function(){
-            output += "<br><hr><br>";
-            output += Person.name + " says hello";
+    // Instantiate new xhr object
+    var request = new XMLHttpRequest();
+    request.open('GET', '../person.txt', true);
+    request.addEventListener('readystatechange', function() {
+        if (request.readyState === 4) {
+            var Person = {}; /* var Person = new Object();   */
+
+            Person = JSON.parse(request.responseText);
+            
+            Person.sayHello = function() {
+                output += "<br><hr><br>" + Person.name + " says hello";
+            }
+
+            // for every key in the Person object, loop...
+            for (var key in Person) {
+
+                // check if the key is the familyNames array
+                if (key === "familyNames") {
+                    output += "<br>Family Names: <br>";
+                    output += "<hr><br>";
+                    output += "<ul>";
+                    for (var index = 0; index < Person.familyNames.length; index++) {
+                        output += "<li>" + Person.familyNames[index] + "</li>";
+                    } // for loop
+                    output += "</ul>";
+                } // if statement
+                else if (key === "sayHello") {
+                    Person.sayHello();
+                }
+                
+                // for all other cases do the following...
+                else {
+
+                    output += Person[key] + "<br>";
+                } // else statement
+
+            } // for in
+
+            firstParagraph.innerHTML = output;
         }
+    });
 
-    }; /* var Person = new Object();   */
+    request.send();
 
-    // for every key in the Person object, loop...
-    for (var key in Person) {
-
-        // check if the key is the familyNames array
-        if (key === "familyNames") {
-           output += "<br>Family Names: <br>";
-            output += "<hr><br>";
-            output += "<ul>";
-            for (var index = 0; index < Person.familyNames.length; index++) {
-                output += "<li>" + Person.familyNames[index] + "</li>";
-            } // for loop
-            output += "</ul>";
-        } // if statement
-        
-        // check if the key is the sayHello Method
-        else if (key === "sayHello") {
-            Person.sayHello();
-        } // else if
-        
-        // for all other cases do the following...
-        else {
-
-            output += Person[key] + "<br>";
-        } // else statement
-    } // for in
-
-    firstParagraph.innerHTML = output;
 
 })();
